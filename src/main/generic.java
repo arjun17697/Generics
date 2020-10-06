393678586c62acd7c9a57ae466d2944c49e80ffe
arjun17697 <arjun17697@gmail.com>
2020-10-06 20:15:36 +0530

    Add:UC2 for determining max float

--- src/main/generic.java
+++ src/main/generic.java
@@ -13,4 +13,14 @@ public class generic {
 	}
 	
 
+	public static <T extends Comparable<T>> T maxfloat_uc2(T[] floatArray) {
+		T max=floatArray[0];
+		for (int i=0;i<3;i++) {
+			if (floatArray[(int) i].compareTo(max)>0) {
+				max= floatArray[i];
+			}
+		}
+		return max;
+	}
+	
 }
--- src/test/GenericJunit.java
+++ src/test/GenericJunit.java
@@ -8,31 +8,56 @@ import org.junit.Test;
 import main.generic;
 
 public class GenericJunit {
-	
 
+//Test Cases for UC1
 	@Test
 	public void returnmaxinteger() {
-		generic tester= new generic();
-		Integer[] intArray=new Integer[] {3,2,1};
-		int result= generic.maxint_uc1(intArray);
-		Assert.assertEquals(3,result);
+		generic tester = new generic();
+		Integer[] intArray = new Integer[] { 3, 2, 1 };
+		int result = generic.maxint_uc1(intArray);
+		Assert.assertEquals(3, result);
 	}
-	
+
 	@Test
 	public void returnmaxinteger1() {
-		generic tester= new generic();
-		Integer[] intArray=new Integer[] {99,128,65};
-		int result= generic.maxint_uc1(intArray);
-		Assert.assertEquals(128,result);
+		generic tester = new generic();
+		Integer[] intArray = new Integer[] { 99, 128, 65 };
+		int result = generic.maxint_uc1(intArray);
+		Assert.assertEquals(128, result);
 	}
 
 	@Test
 	public void returnmaxinteger2() {
-		generic tester= new generic();
-		Integer[] intArray=new Integer[] {89,128,6999};
-		int result= generic.maxint_uc1(intArray);
-		Assert.assertEquals(6999,result);
+		generic tester = new generic();
+		Integer[] intArray = new Integer[] { 89, 128, 6999 };
+		int result = generic.maxint_uc1(intArray);
+		Assert.assertEquals(6999, result);
 	}
 
-}
+//Test Cases for UC2
+	@Test
+	public void returnmaxfloat() {
+		generic tester = new generic();
+		Float[] floatArray = new Float[] { 3.45f, 2.67f, 1.45f };
+		float result = generic.maxfloat_uc2(floatArray);
+		Assert.assertEquals(3.45f, result, 0.000);
 
+	}
+
+	@Test
+	public void returnmaxfloat2() {
+		generic tester = new generic();
+		Float[] floatArray = new Float[] { 3.45f, 8.937f, 1.45f };
+		float result = generic.maxfloat_uc2(floatArray);
+		Assert.assertEquals(8.937f, result, 0.000);
+	}
+
+	@Test
+	public void returnmaxfloat3() {
+		generic tester = new generic();
+		Float[] floatArray = new Float[] { 3.45f, 2.67f, 9.99f };
+		float result = generic.maxfloat_uc2(floatArray);
+		Assert.assertEquals(9.99f, result, 0.000);
+
+	}
+}
